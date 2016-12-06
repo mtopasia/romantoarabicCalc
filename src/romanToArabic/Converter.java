a1426a2dae008791f38609671f30da7d76fbb86b
Mario Topasia <Mario Topasia@172.30.12.21>
2016-12-06 10:30:27 +0200

    check valid romans added

--- src/romanToArabic/Converter.java
+++ src/romanToArabic/Converter.java
@@ -3,21 +3,26 @@ package romanToArabic;
 import java.util.HashMap;
 
 public class Converter {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.println(convertOnechar('V'));
 		System.out.println(convertRomanToArabic("CMIX"));
-		System.out.println(convertRomanToArabic("IC"));
+		System.out.println(convertRomanToArabic("IM"));
 		System.out.println(convertRomanToArabic("MCMXCIX"));
 		System.out.println(convertRomanToArabic("DCCCLXXXVIII"));
-		
+
 		HashMap<Character, Character> map = new HashMap<>();
-		map.put('A', 'B');
-		map.put('C', 'D');
-		map.put('L', 'D');
-		for (Character key : map.keySet()) {
-			System.out.println(key + "-" + map.get(key));
-			
-		}
+		map.put('X', 'I');
+		map.put('C', 'X');
+		map.put('M', 'C');
+		map.put('V', 'I');
+		map.put('L', 'X');
+		map.put('D', 'C');
+		// System.out.println(map.get('M').equals('C'));
+		// for (Character key : map.keySet()) {
+		// System.out.println(key + "-" + map.get(key));
+		// }
+		System.out.println();
+		System.out.println(isNumValid('X', 'L'));
 
 	}
 
@@ -49,26 +54,45 @@ public class Converter {
 			System.out.println("Pole Rooma number");
 			return 0;
 		}
-		
 
 	}
-	private static int convertRomanToArabic(String str){
-		
-		int sum=convertOnechar(str.charAt(str.length()-1));
-		for (int i = 0; i < str.length()-1; i++) {
-		
-			if (convertOnechar(str.charAt(i)) < convertOnechar(str.charAt(i+1)) ){
-				sum-=convertOnechar(str.charAt(i));
-				
+
+	private static int convertRomanToArabic(String str) throws Exception {
+		int sum = convertOnechar(str.charAt(str.length() - 1));
+		for (int i = 0; i < str.length() - 1; i++) {
+			if (convertOnechar(str.charAt(i)) < convertOnechar(str.charAt(i + 1))) {
+				if (isNumValid(str.charAt(i), str.charAt(i + 1))) {
+					sum -= convertOnechar(str.charAt(i));
+				} else {
+					throw new Exception("vigane rooma nr");
+				}
+
+			} else {
+				sum += convertOnechar(str.charAt(i));
+
 			}
-			else {
-				sum+=convertOnechar(str.charAt(i));
-				
-			}
+
 		}
 		return sum;
-		
+	}
+
+	// System.out.println(convertRomanToArabic("CMIX"));
+	// System.out.println(convertRomanToArabic("IC"));
+	// System.out.println(convertRomanToArabic("MCMXCIX"));
+	private static boolean isNumValid(char num1, char num2) {
+		HashMap<Character, Character> map = new HashMap<>();
+		map.put('X', 'I');
+		map.put('C', 'X');
+		map.put('M', 'C');
+		map.put('V', 'I');
+		map.put('L', 'X');
+		map.put('D', 'C');
+
+		// for (Character key : map.keySet()) {
+		// System.out.println(key + "-" + map.get(key));
+		// }
+		return map.get(num2).equals(num1);
+
 	}
 
 }
-
